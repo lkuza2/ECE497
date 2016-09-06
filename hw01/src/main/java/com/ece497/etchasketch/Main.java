2e4909694651ff3a8eeac1a01723a9b8dbcdabb8
Luke Kuza <kuzalj@rose-hulman.edu>
2016-09-06 01:28:45 -0400

    Added hw01 code

---
+++ hw01/src/main/java/com/ece497/etchasketch/Main.java
@@ -0,0 +1,19 @@
+package com.ece497.etchasketch;
+
+import com.ece497.etchasketch.util.MainUtil;
+
+/**
+ * Main class to start entry into the program
+ * @author Luke Kuza
+ */
+public class Main {
+
+    /**
+     * Main entry point for the program
+     * @param args Arguements, unsused
+     */
+    public static void main(String args[]){
+        MainUtil.getInstance().init();
+    }
+
+}
