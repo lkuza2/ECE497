2610699d3effc2c7e76686966d4bb59f8a6e8f5e
Luke Kuza <luke.kuza@allstarwireless.com>
2016-10-25 15:13:23 -0400

    Update gpio_test.c

--- hw07/gpio_test/gpio_test.c
+++ hw07/gpio_test/gpio_test.c
@@ -66,7 +66,7 @@ static int __init ebbgpio_init(void){
    // This next call requests an interrupt line
    result = request_irq(irqNumber,             // The interrupt number requested
                         (irq_handler_t) ebbgpio_irq_handler, // The pointer to the handler function below
-                        IRQF_TRIGGER_RISING,   // Interrupt on rising edge (button press, not release)
+                        IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING,   // Interrupt on rising and falling edge (button press, not release)
                         "ebb_gpio_handler",    // Used in /proc/interrupts to identify the owner
                         NULL);                 // The *dev_id for shared interrupt lines, NULL is okay
 
